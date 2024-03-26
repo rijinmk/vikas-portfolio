41c30f398a750aca27413f2813ddd1a44a5a3184
Rijin Mk <rijin.mk9@gmail.com>
2024-03-26 12:12:59 +0400

    feat: get branch name on nodejs script

--- scripts/workflows/test.js
+++ scripts/workflows/test.js
@@ -1,2 +1,21 @@
-console.log("Hello from nodejs");
-console.log("PROCESS ENV--> ", process.env.EXAMPLE_SECRET);
+console.log("BRANCH NAME FROM NODE: ", process.argv[2]);
+// const branchName = process.argv[2];
+// const regex = /(fix|feat|chore|noticket)\/([\w]+)/;
+// const match = branchName.match(regex);
+// const cardID = match[2];
+// console.log("Trello Card ID from NodeJs", cardID);
+
+// axios(
+//   `https://api.trello.com/1/card/${cardID}?key=${apiKey}&token=${oAuthToken}`,
+//   {
+//     method: "GET",
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   }
+// )
+//   .then((response) => {
+//     return response.data;
+//   })
+//   .then((data) => console.log(JSON.stringify(data, null, 2)))
+//   .catch((err) => console.error(err));
