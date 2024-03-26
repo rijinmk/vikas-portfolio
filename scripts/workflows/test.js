d277d2106c1e448722bcf04a8f03f91e0862b5ce
Rijin Mk <rijin.mk9@gmail.com>
2024-03-26 12:16:00 +0400

    fix: getting trello card details

--- scripts/workflows/test.js
+++ scripts/workflows/test.js
@@ -4,17 +4,17 @@ const match = branchName.match(regex);
 const cardID = match[2];
 console.log("Trello Card ID from NodeJs", cardID);
 
-// axios(
-//   `https://api.trello.com/1/card/${cardID}?key=${apiKey}&token=${oAuthToken}`,
-//   {
-//     method: "GET",
-//     headers: {
-//       Accept: "application/json",
-//     },
-//   }
-// )
-//   .then((response) => {
-//     return response.data;
-//   })
-//   .then((data) => console.log(JSON.stringify(data, null, 2)))
-//   .catch((err) => console.error(err));
+axios(
+  `https://api.trello.com/1/card/${cardID}?key=${apiKey}&token=${oAuthToken}`,
+  {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  }
+)
+  .then((response) => {
+    return response.data;
+  })
+  .then((data) => console.log(JSON.stringify(data, null, 2)))
+  .catch((err) => console.error(err));
