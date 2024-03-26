302fca588942e184e6c118686b5e78ddf00c2a4f
Rijin Mk <rijin.mk9@gmail.com>
2024-03-26 12:44:52 +0400

    feat: moving card to "in pr review"

--- scripts/workflows/test.js
+++ scripts/workflows/test.js
@@ -6,20 +6,21 @@ const match = branchName.match(regex);
 const cardID = match[2];
 console.log("Trello Card ID from NodeJs", cardID);
 
-axios(
-  `https://api.trello.com/1/boards/${process.env.TRELLO_BOARD_ID}/lists?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_AUTH_TOKEN}`,
-  {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  }
-)
+axios
+  .put(
+    `https://api.trello.com/1/cards/${cardID}?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_AUTH_TOKEN}`,
+    {
+      idList: process.env.TRELLO_LIST_IN_PR_REVIEW,
+    }
+  )
   .then((response) => {
-    return response.data;
+    console.log(
+      `Moved card to new list. Response: ${response.status} ${response.statusText}`
+    );
   })
-  .then((data) => console.log(JSON.stringify(data, null, 2)))
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    console.error(err);
+  });
 
 // axios(
 //   `https://api.trello.com/1/card/${cardID}?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_AUTH_TOKEN}`,
