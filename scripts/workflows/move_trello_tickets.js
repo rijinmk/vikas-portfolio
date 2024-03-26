710b412befc9e58e7df73156b0ed5b00c360563c
Rijin Mk <rijin.mk9@gmail.com>
2024-03-26 13:30:40 +0400

    fix: cleaning script

--- scripts/workflows/move_trello_tickets.js
+++ scripts/workflows/move_trello_tickets.js
@@ -5,8 +5,6 @@ const isMerged = process.argv[3] === "merged" ? true : false;
 const regex = /(fix|feat|chore|noticket)\/([\w]+)/;
 const match = branchName.match(regex);
 const cardID = match[2];
-console.log("Trello Card ID from NodeJs", cardID);
-console.log(`isMerged ${isMerged}`);
 
 axios
   .put(
@@ -25,18 +23,3 @@ axios
   .catch((err) => {
     console.error(err);
   });
-
-// axios(
-//   `https://api.trello.com/1/card/${cardID}?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_AUTH_TOKEN}`,
-//   {
-//     method: "GET",
-//     headers: {
-//       Accept: "application/json",
-//     },
-//   }
-// )
-//   .then((response) => {
-//     return response.data;
-//   })
-//   .then((data) => console.log(JSON.stringify(data, null, 2)))
-//   .catch((err) => console.error(err));
