8962ed01b56af9b8c7214fcb7ea683275962170c
Rijin Mk <rijin.mk9@gmail.com>
2024-03-26 12:35:56 +0400

    featL get list ids

--- scripts/workflows/test.js
+++ scripts/workflows/test.js
@@ -7,7 +7,7 @@ const cardID = match[2];
 console.log("Trello Card ID from NodeJs", cardID);
 
 axios(
-  `https://api.trello.com/1/card/${cardID}?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_AUTH_TOKEN}`,
+  `https://api.trello.com/1/boards/${process.env.TRELLO_BOARD_ID}/lists??key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_AUTH_TOKEN}`,
   {
     method: "GET",
     headers: {
@@ -20,3 +20,18 @@ axios(
   })
   .then((data) => console.log(JSON.stringify(data, null, 2)))
   .catch((err) => console.error(err));
+
+// axios(
+//   `https://api.trello.com/1/card/${cardID}?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_AUTH_TOKEN}`,
+//   {
+//     method: "GET",
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   }
+// )
+//   .then((response) => {
+//     return response.data;
+//   })
+//   .then((data) => console.log(JSON.stringify(data, null, 2)))
+//   .catch((err) => console.error(err));
