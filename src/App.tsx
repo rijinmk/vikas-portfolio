8307164d756e98646357704a854686e9a98f2cd2
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-01 23:48:13 +0400

    feat: removed concat

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import CaseStudyPage from './pages/CaseStudyPage/CaseStudyPage'
 function App() {
   const myVar = 'random'
 
-  console.log(myVar + '1')
+  console.log(myVar)
 
   return (
     <Router>
