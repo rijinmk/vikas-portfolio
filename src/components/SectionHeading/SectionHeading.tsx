3aac89c511a2118c0bf4494907cfbd32dd10ad46
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-06 22:23:14 +0400

    feat: added color prop

--- src/components/SectionHeading/SectionHeading.tsx
+++ src/components/SectionHeading/SectionHeading.tsx
@@ -4,18 +4,24 @@ import styles from './SectionHeading.module.css'
 interface ProjectProps {
   index: string
   title: string
-  subtitle: string
+  subtitle?: string
+  color?: string
 }
 
 export default function SectionHeading({
   index,
   title,
-  subtitle
+  subtitle,
+  color
 }: ProjectProps) {
+  const indexColor = `${color}AF`
+
   return (
     <div className={styles.root}>
       <div className={styles.project_title_container}>
-        <span className={styles.section_index}>{index}</span>
+        <span className={`${styles.section_index}`} style={{ color: indexColor }}>
+          {index}
+        </span>
         <h1>{title}</h1>
       </div>
       <h2 className={styles.subtitle}>{subtitle}</h2>
