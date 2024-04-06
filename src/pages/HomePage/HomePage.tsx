c806bf48a16cb0504547115f635caa18b19e4cad
Rijin Mk <rijin.mk9@gmail.com>
2024-04-06 21:41:29 +0400

    feat: 2 links

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
-  return <h1>Homepage</h1>
+  return (
+    <>
+      <h1>Homepage</h1>
+      <Link to='/case-study'>Case Study</Link> <br />
+      <Link to='/component-gallery'>Component Gallery</Link>
+    </>
+  )
 }
 
 export default HomePage
