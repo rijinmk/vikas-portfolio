ac0cf6a33b25d2533e5b5784a1f061078cf662f4
Rijin Mk <rijin.mk9@gmail.com>
2024-04-07 11:29:57 +0400

    feat: skillgrid

--- src/components/SkillGrid/SkillGrid.tsx
+++ src/components/SkillGrid/SkillGrid.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import styles from './SkillGrid.module.css'
 
 interface SkillGridProps {
-    skills: { name: string, image: string }[]
-    spacing: number
+  skills: { name: string; image: string }[]
+  spacing: number
 }
 
-function SkillGrid() {
-  return <h1>Skillgrid deez nuts </h1>
+function SkillGrid({ skills, spacing }: SkillGridProps) {
+  return (
+    <div className={styles.root} style={{ gap: spacing || '20px' }}>
+      {skills.map((skill) => (
+        <div className={styles.cell}>
+          <div
+            style={{
+              backgroundImage: `url(${skill.image})`
+            }}
+            className={styles.cellImage}
+          />
+          <div className={styles.cellText}>{skill.name}</div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default SkillGrid
--- src/pages/ComponentGallery/ComponentGallery.tsx
+++ src/pages/ComponentGallery/ComponentGallery.tsx
@@ -4,24 +4,95 @@ import NameIntro from '../../components/NameIntro/NameIntro'
 import Section from '../../components/Section/Section'
 import SkillGrid from '../../components/SkillGrid/SkillGrid'
 
+const SkillGridData = {
+  skills: [
+    {
+      name: 'AFFINITY MAPPING',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'PERSONAS',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'USABILITY TESTING',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'CARD SORT',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'PROTOTYPE',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'USER INTERVIEW',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'COMPETITIVE ANALYSIS',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'RESPONSIVE DESIGN ',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: '​USER FLOW',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'SKETCHING',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'SYNTHESIZE USER RESEARCH',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'VISUAL DESIGN',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    },
+    {
+      name: 'WIREFRAMING',
+      image:
+        'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg'
+    }
+  ],
+  spacing: 20
+}
+
 function ComponentGallery() {
   return (
     <>
+      {/* SkillGrid Component ------------------------------------- */}
+      <div className={styles.root}>
+        <div className={styles.title}>SkillGrid</div>
+        <div className={styles.componentHolder}>
+          <div className={styles.authorName}>Author: Rijin Mk</div>
+          <SkillGrid {...SkillGridData} />
+        </div>
+      </div>
+
       {/* NameIntro Component ------------------------------------- */}
       <div className={styles.root}>
         <div className={styles.title}>NameIntro</div>
         <div className={styles.componentHolder}>
           <div className={styles.authorName}>Author: Rijin Mk</div>
           <NameIntro name='Rijin Mukundan' subtitle='Front-End Engineer' />
-        </div>
-      </div>
-
-      {/* SkillGrid Component ------------------------------------- */}
-      <div className={styles.root}>
-        <div className={styles.title}>SkillGrid</div>
-        <div className={styles.componentHolder}>
-          <div className={styles.authorName}>Author: Vikas Prakash</div>
-          <SkillGrid />
         </div>
       </div>
 
