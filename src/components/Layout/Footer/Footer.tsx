7a01b1fca185b17726b9126cf514299e0e48ca7c
Rijin Mk <rijin.mk9@gmail.com>
2024-04-23 16:05:53 +0400

    fix: adding the footer back

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import styles from './Footer.module.css'
 
-function Footer() {
-  return <div className={styles.root}>Footer</div>
+interface FooterProps {
+  logo?: string
+  copyrightNotice?: string
+}
+
+function Footer({ logo, copyrightNotice }: FooterProps) {
+  const navigate = useNavigate()
+
+  function navigateHome() {
+    navigate('/')
+    window.scrollTo(0, 0)
+  }
+
+  return (
+    <div className={styles.root}>
+      <button type='button' onClick={navigateHome} className={styles.logo_container}>
+        <img className='logo' src={logo} alt='logo' />
+      </button>
+      <p className={styles.copyrightNotice}>{copyrightNotice}</p>
+    </div>
+  )
 }
 
 export default Footer
