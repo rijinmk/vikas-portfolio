a68d8e6e219be15f2ae06b6c6a1864a2ece823da
tldayan <110288123+tldayan@users.noreply.github.com>
2024-04-24 23:40:18 +0400

    fix: resolved_comments

--- src/components/CaseStudyTile/CaseStudyTile.tsx
+++ src/components/CaseStudyTile/CaseStudyTile.tsx
@@ -15,10 +15,10 @@ export default function CaseStudyTile({ projects }: CaseStudyTileProps) {
   return (
     <div className={styles.root}>
       {projects.map((eachProject) => (
-        <div className={styles.caseStudyContainer}>
-          <img style={{ order: eachProject.direction === 'left' ? 0 : 1 }} src={eachProject.imageUrl} alt={`${eachProject.title}`} />
+        <div key={eachProject.title} className={styles.caseStudyContainer}>
+          <img style={{ order: eachProject.direction === 'left' ? 0 : 1 }} src={eachProject.imageUrl || '../../assets/images/jpg/defaultProjectImage.jpg'} alt={`${eachProject.title}`} />
           <div className={styles.caseStudyInfo}>
-            <h3>{eachProject.title}</h3>
+            {eachProject.title && <h3>{eachProject.title}</h3>}
             <p className={styles.subtitle}>{eachProject.subtitle}</p>
             <p className={styles.project_type}>{eachProject.projectType}</p>
           </div>
